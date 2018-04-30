2ff7f469fedc3374cbf7b9a5d5c1074f1c2c0b8b
wan <wan>
2018-04-30 20:47:24 +0800

    aa

---
+++ src/main/java/com/example/demo/TreeNode.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class TreeNode {
+    int value;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    TreeNode left;
+    TreeNode right;
+
+}
